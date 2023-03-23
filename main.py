75489f923c9c421a6e3e4fdd6b12cd035157adfc
elDiego04 <diego.salazarc4@gmail.com>
2023-03-22 19:56:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import opreaciones as o
+
+def main():
+    sum = o.sumar(10, 2)
+    res = o.restar(10, 2)
+    mult = o.multiplicar(10, 2)
+    divi = o.dividir(10, 2)
+
+    print('hola', sum, res, mult, divi)
+
+if __name__ == '__main__':
+    main()
